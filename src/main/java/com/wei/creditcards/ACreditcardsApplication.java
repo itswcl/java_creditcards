11f0a893093a03ef8de1e9446681e533496f20c6
weicheng lee <z82111712@gmail.com>
2021-12-20 11:11:32 -0800

    initial credit cards project

---
+++ src/main/java/com/wei/creditcards/ACreditcardsApplication.java
@@ -0,0 +1,13 @@
+package com.wei.creditcards;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ACreditcardsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ACreditcardsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wei/creditcards/ACreditcardsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wei.creditcards;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ACreditcardsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
